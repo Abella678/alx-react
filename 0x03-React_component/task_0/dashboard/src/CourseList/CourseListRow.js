c8d6c4e9693a60ea1dc80c35d633cf4d5985f1f0
Abel Mengistu <abelmemory0@gmail.com>
2023-03-01 10:10:07 +0000

    task

---
+++ 0x03-React_component/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function CourseListRow(props) {
+  if (props.isHeader) {
+    if (props.textSecondCell == null) {
+      return <tr><th colSpan="2">{props.textFirstCell}</th></tr>
+    } else {
+      return (
+        <tr>
+          <th>{props.textFirstCell}</th>
+          <th>{props.textSecondCell}</th>
+        </tr>
+        )
+    }
+  } 
+  return (
+    <tr>
+      <td>{props.textFirstCell}</td>
+      <td>{props.textSecondCell}</td>
+    </tr>
+
